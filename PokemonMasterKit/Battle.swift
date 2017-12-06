60e672faba1bbacb4a4a411e185e983cd329584e
Kensuke Hoshikawa <kensuke-hoshikawa@cookpad.com>
2017-12-06 23:18:35 +0900

    add public

---
+++ PokemonMasterKit/Battle.swift
@@ -0,0 +1,9 @@
+//
+//  Battle.swift
+//  PokemonMasterKit
+//
+//  Created by kensuke-hoshikawa on 2017/12/06.
+//  Copyright © 2017年 star__hoshi. All rights reserved.
+//
+
+import Foundation
--- PokemonMasterKit/Pokemon.swift
+++ PokemonMasterKit/Pokemon.swift
@@ -11,17 +11,21 @@ import Foundation
 public protocol PokemonProtocol {
     var name: String { get }
     var baseStats: BaseStats { get }
+    var effortValues: EffortValues { get set }
+    var individualValues: IndividualValues { get set }
+    var nature: Nature { get set }
+    var level: Int { get set }
 }
 
 public class Pokemon {
-    let name: String
-    let baseStats: BaseStats
-    var effortValues: EffortValues
-    var individualValues: IndividualValues
-    var nature: Nature
-    var level: Int = 50
+    public let name: String
+    public let baseStats: BaseStats
+    public var effortValues: EffortValues
+    public var individualValues: IndividualValues
+    public var nature: Nature
+    public var level: Int = 50
 
-    init(name: String, baseStats: BaseStats, effortValues: EffortValues, individualValues: IndividualValues, nature: Nature) {
+    public init(name: String, baseStats: BaseStats, effortValues: EffortValues, individualValues: IndividualValues, nature: Nature) {
         self.name = name
         self.baseStats = baseStats
         self.effortValues = effortValues
@@ -29,28 +33,28 @@ public class Pokemon {
         self.nature = nature
     }
 
-    var hp: Int {
+    public var hp: Int {
         let temp = Float((baseStats.hp * 2) + individualValues.hp + (effortValues.hp / 4)) * (Float(level) / 100)
         return Int(temp) + level + 10
     }
 
-    var attack: Int {
+    public var attack: Int {
         return calcStat(baseStat: baseStats.attack, individualValue: individualValues.attack, effortValue: effortValues.attack, natureEffectType: nature.effect.attack)
     }
 
-    var defense: Int {
+    public var defense: Int {
         return calcStat(baseStat: baseStats.defense, individualValue: individualValues.defense, effortValue: effortValues.defense, natureEffectType: nature.effect.defense)
     }
 
-    var specialAttack: Int {
+    public var specialAttack: Int {
         return calcStat(baseStat: baseStats.specialAttack, individualValue: individualValues.specialAttack, effortValue: effortValues.specialAttack, natureEffectType: nature.effect.specialAttack)
     }
 
-    var specialDefense: Int {
+    public var specialDefense: Int {
         return calcStat(baseStat: baseStats.specialDefense, individualValue: individualValues.specialDefense, effortValue: effortValues.specialDefense, natureEffectType: nature.effect.specialDefense)
     }
 
-    var speed: Int {
+    public var speed: Int {
         return calcStat(baseStat: baseStats.speed, individualValue: individualValues.speed, effortValue: effortValues.speed, natureEffectType: nature.effect.speed)
     }
 
